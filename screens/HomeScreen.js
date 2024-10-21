9f3a4c495cb509a161cddb6b4ffad8ed1f3c6640
anhng1106 <ntvanh1106@gmail.com>
2024-10-21 22:54:26 +0300

    Created light/dark theme for homepage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import {
   View,
   Text,
@@ -8,11 +8,16 @@ import {
   TouchableOpacity,
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import { ThemeContext } from "../ThemeContext";
 
 const HomeScreen = ({ navigation }) => {
   const handleProfile = () => {
     navigation.navigate("ProfileScreen"); // Navigate to a profile screen (you should create this)
   };
+
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  const styles = theme === "light" ? lightTheme : darkTheme;
 
   return (
     <View style={styles.container}>
@@ -42,11 +47,12 @@ const HomeScreen = ({ navigation }) => {
   );
 };
 
-const styles = StyleSheet.create({
+// Light theme styles
+const lightTheme = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 40, // Adjust depending on the status bar height
+    backgroundColor: "#fdeae2", // Light theme background
+    paddingTop: 40,
   },
   topBar: {
     flexDirection: "row",
@@ -72,7 +78,7 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     paddingHorizontal: 20,
     height: 30,
-    backgroundColor: "#f9f9f9",
+    backgroundColor: "#f9f9f9", // Light background for search
     fontSize: 16,
     width: "60%",
     alignItems: "center",
@@ -84,7 +90,60 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   icon: {
-    color: "#000",
+    color: "#000", // Black icon for light theme
+    marginRight: -10,
+    marginTop: -33,
+  },
+  content: {
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+});
+
+// Dark theme styles
+const darkTheme = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#545454", // Dark theme background
+    paddingTop: 40,
+  },
+  topBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingBottom: 10,
+  },
+  logo: {
+    width: 100,
+    height: 50,
+    width: "35%",
+    marginTop: -30,
+    marginLeft: -30,
+  },
+  searchContainer: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+  searchBar: {
+    borderWidth: 1,
+    borderColor: "#444", // Darker border for dark theme
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    height: 30,
+    backgroundColor: "#333", // Dark background for search
+    fontSize: 16,
+    width: "60%",
+    alignItems: "center",
+    alignSelf: "center",
+    marginTop: -52,
+    marginLeft: 50,
+  },
+  iconContainer: {
+    padding: 5,
+  },
+  icon: {
+    color: "#fff", // White icon for dark theme
     marginRight: -10,
     marginTop: -33,
   },
