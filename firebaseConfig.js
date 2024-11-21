bcd5e4fe70e73c16d49626d131bb1f7b240b64ee
anhng1106 <ntvanh1106@gmail.com>
2024-11-21 11:50:28 +0200

    enable persistent authentication state for Firebase Authentication in React Native

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAss8YyS-Rml70B8_cvmwm3BTZnMJyRJsw",
@@ -14,6 +15,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and Firestore
-export const auth = getAuth(app);
+// Initialize Firebase Authentication with AsyncStorage for persistence
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// Initialize Firestore
 export const db = getFirestore(app); // Export Firestore instance
